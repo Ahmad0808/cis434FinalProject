4f75925420c990801fb57948b31168934fa389ce
EwiSkywalker <etgulley65@gmail.com>
2024-04-29 14:49:26 -0400

    Scene stuff

---
+++ FrogsAndWorms/src/main/java/SceneManager.java
@@ -0,0 +1,44 @@
+import com.example.frogsandworms.scene;
+import javafx.scene.image.Image;
+
+public class SceneManager {
+    public scene[] scenes;
+
+    public SceneManager(){
+        scenes = new scene[10];
+
+    }
+
+    public scene sceneDungeon1() {
+        scene s = new scene();
+        s.sceneID = 1;
+        s.image = new Image(getClass().getResourceAsStream("dungeonscene1.jpg"));
+        s.text = "The putrid stench of rotting flesh filled my amphibious nose as death surrounds me in the lair of the Skrott. My limbs are shackled and my equipment has been taken away. Good thing I have put lots of practice into:";
+        s.btn1 = "Strength";
+        s.btn2 = "Luck";
+        s.btn3 = "Intelligence";
+        return s;
+    }
+
+    public scene sceneDungeon2() {
+        scene s = new scene();
+        s.sceneID =2;
+        s.image = new Image(getClass().getResourceAsStream("dungeonscene1.jpg"));
+        s.text = "The Frosks will need their Bog-Lord, what shall I do?";
+        s.btn1 = "Wait";
+        s.btn2 = "Break-Free";
+        s.btn3 = "";
+        return s;
+    }
+
+    public scene sceneDungeon3() {
+        scene s = new scene();
+        s.sceneID =3;
+        s.image = new Image(getClass().getResourceAsStream("dungeonWSword.jpg"));
+        s.text = "I have found my trusty sword - Zephyr. The sword will protect me but it is all about the wielder. I need to escape so I can replenish my strength. For now I am about as weak as an average Bog-Walker. There is a dimly-lit cavernous path on my right or a wooden door with light breaching out below it:";
+        s.btn1 = "Dimly-lit Path";
+        s.btn2 = "Door";
+        s.btn3 = "";
+        return s;
+    }
+}
--- FrogsAndWorms/src/main/java/scene.java
+++ FrogsAndWorms/src/main/java/scene.java
@@ -2,38 +2,14 @@ package com.example.frogsandworms;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 
-public class scene{
-	int sceneID;
-	Image image;
-	String text;
-	String btn1;
-	String btn2;
-	String btn3;
-	
-	
-	public void sceneDungeon1() {
-		this.sceneID = 1;
-		this.image = new Image(getClass().getResourceAsStream("dungeonScene1.jpg"));
-		this.text = "The putrid stench of rotting flesh filled my amphibious nose as death surrounds me in the lair of the Skrott. My limbs are shackled and my equipment has been taken away. Good thing I have put lots of practice into:";
-		this.btn1 = "Strength";
-		this.btn2 = "Luck";
-		this.btn3 = "Intelligence";
+public class scene {
+	public int sceneID;
+	public Image image;
+	public String text;
+	public String btn1;
+	public String btn2;
+	public String btn3;
+
+	public scene(){
 	}
-	
-	public void sceneDungeon2() {
-		this.sceneID =2;
-		this.image = new Image(getClass().getResourceAsStream("dungeonScene1.jpg"));
-		this.text = "The Frosks will need their Bog-Lord, what shall I do?";
-		this.btn1 = "Wait";
-		this.btn2 = "Break-Free";
-		this.btn3 = "";
-	}
-	
-	public void sceneDungeon3() {
-		this.sceneID =3;
-		this.image = new Image(getClass().getResourceAsStream("dungeonWSword.jpg"));
-		this.text = "I have found my trusty sword - Zephyr. The sword will protect me but it is all about the wielder. I need to escape so I can replenish my strength. For now I am about as weak as an average Bog-Walker. There is a dimly-lit cavernous path on my right or a wooden door with light breaching out below it:";
-		this.btn1 = "Dimly-lit Path";
-		this.btn2 = "Door";
-		this.btn3 = "";
-	}
+}
