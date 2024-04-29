9242943bc498c3517fd644bc593296e8e0672882
jstephans1 <jacob.stephans32@gmail.com>
2024-04-29 14:50:58 -0400

    Update scene.java

--- FrogsAndWorms/src/main/java/scene.java
+++ FrogsAndWorms/src/main/java/scene.java
@@ -2,14 +2,15 @@ package com.example.frogsandworms;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 
-public class scene {
-	public int sceneID;
-	public Image image;
-	public String text;
-	public String btn1;
-	public String btn2;
-	public String btn3;
-
-	public scene(){
+public class scene{
+	int sceneID;
+	Image image;
+	String text;
+	String btn1;
+	String btn2;
+	String btn3;
+	
+	
+	
+	
 	}
-}
